005bfddba4275ef0d1b28035bb46edc0e15a80a6
vs <santi.anjm@gmail.com>
2023-03-16 23:05:33 +0200

    Traffic

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -13,38 +13,63 @@ namespace ConsoleApp5
                 Height = globalHeight,
                 Width = globalWidth,
                 AddTraffic = true,
-                TrafficSeed = 17
+                TrafficSeed = 14
             });
             
             string[,] map = generator.Generate();
-            var toStart = new Point(0, 0);
+            var Start = new Point(0, 0);
             var finish = new Point(globalWidth - 2, globalHeight - 2);
-            List<Point> path = GetShortestPath(map, toStart, finish);
+            List<Point> path = GetShortestPath(map, Start, finish);
             new MapPrinter().Print(map, path);
 
             List<Point> GetShortestPath(string[,] localMap, Point start, Point goal)
             {
-                var endPoint = goal;
+                var lastPoint = goal;
                 var frontier = new Queue<Point>();
-                var locPath = new List<Point> { start };
+                var localPath = new List<Point> { start };
+                var origins = new Dictionary<Point, Point>();
+                var distances = new Dictionary<Point, int>();
                 frontier.Enqueue(start);
+                distances.Add(start, 0);
                 while (frontier.Count != 0)
                 {
                     var current = frontier.Dequeue();
                     var availablePoint = SearchNearbyPoints(localMap, current);
                     foreach (var point in availablePoint)
                     {
-                        if  ;
+                        if (!origins.ContainsKey(point))
+                        {
+                            frontier.Enqueue(point);
+                            origins.Add(point, current);
+                            if (!distances.ContainsKey(point))
+                            {
+                                distances.Add(point, distances[current]+1);
+                            }
+                        }
+                        else if (origins.ContainsKey(point) && distances[current] + 1 < distances[point])
+                        {
+                            origins[point] = current;
+                        }
+                    }
+
+                    if (current.Equals(goal))
+                    {
+                        lastPoint = goal;
+                        break;
                     }
                 }
+
+                var lenth = distances[lastPoint];
+                for (var i = 0; i != lenth; i++)
+                {
+                    localPath.Add(origins[lastPoint]);
+                    lastPoint = origins[lastPoint];
+                }
+                localPath.Add(goal);
+                return localPath;
             }
 
-
-
-
-
-
-            List<Point> SearchNearbyPoints(string[,] localMap2, Point current)
+        List<Point> SearchNearbyPoints(string[,] localMap2, Point current)
             {
                 List<Point> available = new List<Point>();
                 if (current.Column - 1 >= 0 && current.Column - 1 <= globalWidth - 1 && current.Row <= globalWidth - 1)
